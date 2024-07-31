75cf7378b6ce9f0a10c038944bfda4cc109c27cc
Sukhress Warun <104361411+Sukhress-Warun@users.noreply.github.com>
2024-07-31 09:07:36 +0000

    audio tag

--- src/index.jsx
+++ src/index.jsx
@@ -4,14 +4,9 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
-function playSound() {
-  const audio = new Audio("../../assets/backgroundMusic.mp3");
-  audio.play().then(r => console.log(r));
-}
 
 ReactDOM.render(
   <BrowserRouter>
-      {setTimeout(playSound, 1000)}
     <App />
   </BrowserRouter>,
   document.getElementById("root")
