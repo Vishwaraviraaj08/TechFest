bb15aa232e5f639a28c2ecb966605efa9fb643d9
sujay r <sujaysy0006@gmail.com>
2024-08-09 00:45:46 +0530

    changed rounds

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -39,6 +39,7 @@ const ContactUs = () => {
   return (
     <div className={classes.background}>
       <div style={{ marginTop: '10%' }}>&nbsp;</div>
+
       <h1 style={{ color: 'white', fontSize: '4rem', margin: '10% 3% 3% 3%' }}>OFFICE BEARERS</h1>
       <div className={"profile-secretary"}
            style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '10%' }}>
@@ -86,6 +87,35 @@ const ContactUs = () => {
           backgroundCoverImage: "https://img.freepik.com/premium-photo/hallway-with-neon-lights-wall_787273-1422.jpg"
         }}/>
       </div>
+      {/* <h1 style={{ color: 'white', fontSize: '4rem', margin: '10% 3% 3% 3%' }}>STAFF CO-ORDINATORS</h1>
+      <div className={"profile-secretary"}
+           style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '10%' }}>
+        <Profile data={{
+          name: "Valarmathi",
+          imageUrl: "/Organizers/Obli.png",
+          designation: "Secretary",
+          phone: "9655236550",
+          mail: "sec21it109@sairamtap.edu.in",
+          backgroundCoverImage: "https://th.bing.com/th/id/OIP.d0P6wkVcQXrflDByh9QD9wAAAA?rs=1&pid=ImgDetMain"
+        }}/>
+        <Profile data={{
+          name: "Vasanthan ",
+          imageUrl: "/Organizers/Hari.png",
+          designation: "Secretary",
+          phone: "9342801642",
+          mail: "sec21it052@sairamtap.edu.in",
+          backgroundCoverImage: "https://th.bing.com/th/id/OIP.vten8hXB10zmVRpJKmCfRQHaE9?w=626&h=419&rs=1&pid=ImgDetMain"
+        }}/>
+        <Profile data={{
+          name: "Naveen Kumar",
+          imageUrl: "/Organizers/Naveen.jpg",
+          designation: "Secretary",
+          phone: "6374974153",
+          mail: "sec21it057@sairamtap.edu.in",
+          backgroundCoverImage: "https://img.freepik.com/premium-photo/hallway-with-neon-lights-wall_787273-1422.jpg"
+        }}/>
+      </div> */}
+
       <div className={classes.card}>
         <div className={classes.card2}>
           <h1 className={classes.heading}>Contact Us</h1>
